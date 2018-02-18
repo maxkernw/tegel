06ab4b59c61232f7b923bcef6540179ef3d793b7
maxkernw <maxkernw@gmail.com>
2018-02-18 14:07:38 +0100

    fff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('collapse', [
+      state('open', style({
+        opacity: '1',
+        display: 'block',
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('closed',   style({
+        opacity: '0',
+        display: 'none',
+        transform: 'translate3d(0, -100%, 0)'
+      })),
+      transition('closed => open', animate('200ms ease-in')),
+      transition('open => closed', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
   isNavbarCollapsed = true;
+  collapse:string = "closed";
+
+  toggleCollapse() {
+    this.collapse = this.collapse == "open" ? 'closed' : 'open';
+
+  }
   constructor(private authService: AuthService) {}
 
   logout(){
